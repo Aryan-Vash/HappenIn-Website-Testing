db30f0c9b47c7139c444aaf03c3921980f00d2e1
Atin1704 <162487111+Atin1704@users.noreply.github.com>
2025-04-14 13:14:48 +0530

    Update serializers.py

--- serializers.py
+++ serializers.py
@@ -580,4 +580,12 @@ class EventUpdateSerializer(serializers.ModelSerializer):
         return data
 
 
+#35
+class EventRegistrationSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+    email = serializers.EmailField(source='user.emailID', read_only=True)
+    contact = serializers.CharField(source='user.contactNo', read_only=True)
 
+    class Meta:
+        model = Registration
+        fields = ['id', 'username', 'email', 'contact']
